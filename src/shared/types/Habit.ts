8ada84f9ce0057a032b287bc93ca0f541c406e90
Le Dan <leyendanwork@gmail.com>
2026-06-12 13:22:25 +0700

    created[goalCalculator]: caculate streak/total completion progress, current streak, longest streak, total completion

--- src/shared/types/Habit.ts
+++ src/shared/types/Habit.ts
@@ -1,6 +1,12 @@
 export interface Habit {
     id: string;
     name: string;
-    description?: string;
-    streak: number;
+    priority:Priority,
+    frequencyType: FrequencyType,
+    status: HabitStatus,
+    targetPerDay: number;
 }
+
+type Priority = "LOW" | "MEDIUM" | "HIGHT"
+type FrequencyType = "DAY_OF_WEEK" | "DAILY"
+type HabitStatus = "PAUSED" | "RESUMED" | "ARCHIVED" | "ACTIVE"
